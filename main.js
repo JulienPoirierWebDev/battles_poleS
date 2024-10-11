bb0b1283aa7a4e111f8c11de8b80af96f93e9d39
Julien POIRIER <connexion@julienpoirier-Webdev.com>
2024-10-11 16:15:00 +0200

    add inscription form

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ import './style.css';
 
 const divBattles = document.querySelector('#battles');
 const formLogin = document.querySelector('#formLogin');
+const formRegister = document.querySelector('#formRegister');
+const emailRegisterInput = document.querySelector('#email-register');
+const passwordRegisterInput = document.querySelector('#password-register');
+const nameRegisterInput = document.querySelector('#name-register');
 const passwordInput = document.querySelector('#password');
 const emailInput = document.querySelector('#email');
 const userDiv = document.querySelector('#user');
@@ -100,3 +104,38 @@ const localToken = localStorage.getItem('token');
 if (localToken) {
 	formLogin.classList.toggle('hidden');
 }
+
+formRegister.addEventListener('submit', (event) => {
+	event.preventDefault();
+	const password = passwordRegisterInput.value;
+	const email = emailRegisterInput.value;
+	const name = nameRegisterInput.value;
+
+	const options = {
+		method: 'POST',
+		body: JSON.stringify({ password, email, name }),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	};
+
+	fetch(
+		'https://api.which-one-battle.julienpoirier-webdev.com/api/users',
+		options
+	)
+		.then((response) => {
+			if (response.ok) {
+				response.json().then((data) => {
+					console.log(data);
+					localStorage.setItem('token', data.token);
+					userDiv.innerHTML = `<p>Bonjour ${data.user.name} !!</p>`;
+					formRegister.classList.add('hidden');
+				});
+			} else {
+				alert('Il y a une erreur avec votre inscription');
+			}
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+});
